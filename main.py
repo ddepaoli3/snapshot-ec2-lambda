3c41ec9bdc5b482b037209485406f71df505f679
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2016-12-23 11:38:20 +0100

    Create AMI of all ec2

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import boto3
 import os
 import sys
+import datetime
+
 
 region = 'eu-west-1'
 use_iam_role = False
@@ -16,10 +18,12 @@ try:
         aws_secret_access_key = os.environ['AWS_ACCESS_KEY_ID']
         session = boto3.Session(aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
         ec2 = session.resource('ec2')
-        #client = boto3.client('ec2', aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
 except Exception as e:
     print "Set AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID env or set IAM role to instance that execute this script"
     sys.exit(-1)
 
 for i in ec2.instances.all():
-    print(i)
+    today = datetime.date.today()
+    date = str(today)
+    image_name = i.id + '-' +  date
+    i.create_image(Name=image_name, InstanceId=i.id )
