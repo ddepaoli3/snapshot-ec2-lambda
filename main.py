d4077db6a5539b9a39d011e4d611b6f04a7a211b
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2016-12-23 10:36:46 +0100

    Option to use IAM role

--- main.py
+++ main.py
@@ -2,16 +2,20 @@
 
 import boto3
 import os
+import sys
 
 region = 'eu-west-1'
+use_iam_role = False
 
-botosession = boto3.session.Session(region_name=region)
 try:
-    aws_secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
-    aws_access_key = os.environ['AWS_ACCESS_KEY_ID']
+    if use_iam_role:
+        client = boto3.client('ec2')
+    else:
+        aws_access_key_id = os.environ['AWS_SECRET_ACCESS_KEY']
+        aws_secret_access_key = os.environ['AWS_ACCESS_KEY_ID']
+        client = boto3.client('ec2', aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
 except Exception as e:
-    print "Set AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID env"
+    print "Set AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID env or set IAM role to instance that execute this script"
     sys.exit(-1)
 
-client = boto3.client('ec2')
 print client.describe_volumes()
