8e490e47047f7899d0ccfa32698945c10ef8c370
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2016-12-23 10:50:21 +0100

    Use ec2 resource

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ use_iam_role = False
 
 try:
     if use_iam_role:
-        client = boto3.client('ec2')
+        session = boto3.Session()
+        ec2 = session.resource('ec2')
     else:
         aws_access_key_id = os.environ['AWS_SECRET_ACCESS_KEY']
         aws_secret_access_key = os.environ['AWS_ACCESS_KEY_ID']
-        client = boto3.client('ec2', aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
+        session = boto3.Session(aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
+        ec2 = session.resource('ec2')
+        #client = boto3.client('ec2', aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
 except Exception as e:
     print "Set AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID env or set IAM role to instance that execute this script"
     sys.exit(-1)
 
-print client.describe_volumes()
+for i in ec2.instances.all():
+    print(i)
