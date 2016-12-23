40f3922f6c886020806ea7d9bf36425bbe3e55b5
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2016-12-23 17:30:10 +0100

    Delete function discover ami older than x days and remove them

--- main.py
+++ main.py
@@ -3,25 +3,37 @@
 import os
 import sys
 import datetime
+from datetime import datetime, timedelta
 from EC2manager import EC2manager
 
 region = 'eu-west-1'
 use_iam_role = False
 
-def main():
-    try:
-        if use_iam_role:
-            ec2_manager = EC2manager()
-        else:
-            aws_access_key_id = os.environ['AWS_SECRET_ACCESS_KEY']
-            aws_secret_access_key = os.environ['AWS_ACCESS_KEY_ID']
-            ec2_manager = EC2manager(aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
-    except Exception as e:
-        print "Set AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID env or set IAM role to instance that execute this script"
-        sys.exit(-1)
+try:
+    if use_iam_role:
+        ec2_manager = EC2manager()
+    else:
+        aws_access_key_id = os.environ['AWS_SECRET_ACCESS_KEY']
+        aws_secret_access_key = os.environ['AWS_ACCESS_KEY_ID']
+        ec2_manager = EC2manager(aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
+except Exception as e:
+    print "Set AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID env or set IAM role to instance that execute this script"
+    sys.exit(-1)
 
-    print ec2_manager.get_all_ami()
+def delete_older_than_given_days(days):
+    delete_time = datetime.utcnow() - timedelta(days=days)
+    for ami in ec2_manager.get_all_ami():
+        created_ami_time = datetime.strptime(ami['CreationDate'], '%Y-%m-%dT%H:%M:%S.000Z')
+        if created_ami_time < delete_time:
+            print "da cancellare ami " + ami["ImageId"]
+
+def main():
+    pass
+    #ec2_manager.create_ami_all_instances()
+    #print ec2_manager.get_all_ami()
+    #ec2_manager.remove_ami("ami-efc8eb9c")
+
 
 
 if __name__ == '__main__':
-    main()
+    delete_older_than_given_days(5)
