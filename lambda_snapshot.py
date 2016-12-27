bbd49287a35e9944b8ecf8ea7cf6001332220998
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2016-12-27 15:23:09 +0100

    Implements function for ami:
    * snapshot all instances
    * get ami list

--- lambda_snapshot.py
+++ lambda_snapshot.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from EC2manager import EC2manager
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Snapshot Lambda app!", 200
+
+@app.route('/ami')
+def get_all_ami():
+    try:
+        ec2_manager = EC2manager()
+        all_ami = str(ec2_manager.get_all_ami())
+    except Exception:
+        all_ami = "Error in get AMI list"
+    return all_ami, 200
+
+@app.route('/snapshotall')
+def snapshot_all():
+    try:
+        ec2_manager = EC2manager()
+        output = ec2_manager.create_ami_all_instances()
+    except Exception:
+        output = "Error. Impossible to snapshot instances"
+    return str(output), 200
 
 if __name__ == '__main__':
     app.run()
