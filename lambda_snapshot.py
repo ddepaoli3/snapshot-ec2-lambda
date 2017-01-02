20608c5ca012e5533cc15ccf68ae1407b589ed04
Giuseppe Cossu <giuseppe.cossu@xpeppers.com>
2017-01-02 17:15:15 +0100

    Add fixed function for cron use remove cron warm up

--- lambda_snapshot.py
+++ lambda_snapshot.py
@@ -43,5 +43,20 @@ def delete_older_than_given_days_api(event=None, context=None, days_num=0):
             delete_ami_list.append(ami["ImageId"])
     return str(delete_ami_list), 200
 
+def delete_older_than_given_days(event=None, context=None):
+    days_num=8
+    try:
+        ec2_manager = EC2manager()
+    except Exception:
+        return "Impossible to create ec2 session. Cannot continue", 200
+    delete_time = datetime.utcnow() - timedelta(days=int(days_num))
+    delete_ami_list = []
+    for ami in ec2_manager.get_all_ami(Filters=[{"Name":"tag-key", "Values":["CreatedByBackupScript"]}]):
+        created_ami_time = datetime.strptime(ami['CreationDate'], '%Y-%m-%dT%H:%M:%S.000Z')
+        if created_ami_time < delete_time:
+            ec2_manager.remove_ami(ami["ImageId"], DryRun=False)
+            delete_ami_list.append(ami["ImageId"])
+    return str(delete_ami_list), 200
+
 if __name__ == '__main__':
     app.run()
